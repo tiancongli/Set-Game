f56170eaebf3ea1e2b95e445efa029faa1b24309
tiancongli <ltiancong@gmail.com>
2023-11-18 19:00:46 +1100

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Tim Li on 18/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Tim Li on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
